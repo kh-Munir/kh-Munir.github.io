33cd7c566140d8904fe782cc081b9a72059ca25f
kh-Munir <khondokermunir@gmail.com>
2025-04-06 18:33:48 -0400

    dir added

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // 👈 set your actual repo name here (important for GitHub Pages)
-  plugins: [react()],
+ plugins: [react()],
   build: {
-    outDir: 'kh-Munir.github.io' // optional, default is 'dist'
+    outDir: 'dist' // optional, default is 'dist'
   }
 })
