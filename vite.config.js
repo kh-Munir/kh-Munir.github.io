8ce25fb2239add8829cf1be96337426e765934ae
kh-Munir <khondokermunir@gmail.com>
2025-04-06 18:17:52 -0400

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/your-repo-name/', // 👈 set your actual repo name here (important for GitHub Pages)
+  plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'kh-Munir.github.io' // optional, default is 'dist'
   }
 })
